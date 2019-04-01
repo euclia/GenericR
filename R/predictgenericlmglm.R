373dfbf11aa865065de8603df769f47b64b81337
Periklis_ts <ptsirostsib@gmail.com>
2019-04-01 13:04:12 +0300

    Update predictgenericlmglm.R

--- R/predictgenericlmglm.R
+++ R/predictgenericlmglm.R
@@ -5,21 +5,32 @@
 
 
 PredictBaseLmGlm <- function(dataset, rawModel, additionalInfo){
+  # Get feature keys (a key number that points to the url)
   feat.keys <-  dataset$features$key
+  # Get feature names (actual name)
   feat.names <- dataset$features$names
+  # Convert names from a factor list to a vector of characters
   feat.names <- as.vector(unlist(lapply(feat.names, as.character)))
+  # Create a dataframe that includes the feature key and the corresponding name
   key.match <- data.frame(cbind(feat.keys, feat.names), stringAsFactors = FALSE)
   
+  # Initialize a dataframe with as many rows as the number of values per feature
   rows_data <- length(dataset$dataEntry$values[,2])
   df <- data.frame(matrix(0, ncol = 0, nrow = rows_data))
-
+ 
   for(key in feat.keys){
+    # For each key (feature) get the vector of values (of length 'row_data')
     feval <- dataset$dataEntry$values[i][,1]
+    # Name the column with the corresponding name that is connected with the key
     df[key.match[key.match$feat.keys == i, 2]] <- feval
   }
+  # Unserialize the model
   mod <- unserialize(base64_dec(rawModel))
   model <- mod$MODEL
+  # Extract the predicted value names
   predFeat <- additionalInfo$predictedFeatures[1][[1]]
+  # Make the prediction using the model and the new data
+  # Note that the names of the dataframe must be the same with the original
   predictions <- predict(model, df)
   for(i in 1:length(predictions)){
     prediction<- data.frame(predictions[i])
