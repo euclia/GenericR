56296bbf948e3f66a9dbc7b8c4d149da07459d79
Periklis_ts <ptsirostsib@gmail.com>
2021-07-16 16:43:01 +0300

    Includes Ensemble

--- R/predict_caret.R
+++ R/predict_caret.R
@@ -5,6 +5,11 @@
 #'
 
 jaqpot.predict.caret <- function(dataset, rawModel, additionalInfo){
+
+  #################################
+  ## Input retrieval from Jaqpot ##
+  #################################
+
   # Get feature keys (a key number that points to the url)
   feat.keys <-  dataset$features$key
   # Get feature names (actual name)
@@ -24,18 +29,34 @@ jaqpot.predict.caret <- function(dataset, rawModel, additionalInfo){
     df[key.match[key.match$feat.keys == key, 2]] <- feval
   }
 
-  # Unserialize the model
-  mod <- unserialize(jsonlite::base64_dec(rawModel))
-  model <- mod$MODEL
-  preprocess <- mod$PREPROCESS
-  extra.args <- mod$extra.args[[1]]
-
-
   # Extract the predicted value names
   predFeat <- additionalInfo$predictedFeatures[1][[1]]
   # Make the prediction using the model and the new data
   # Note that the names of the dataframe must be the same with the original
 
+
+  ###########################
+  ## Model unserialization ##
+  ###########################
+
+  mod <- unserialize(jsonlite::base64_dec(rawModel))
+  model <- mod$MODEL
+  preprocess <- mod$PREPROCESS
+  ensemble <- mod$ENSEMBLE
+  if (is.null(mod$extra.args)){
+    extra.args <- mod$extra.args[[1]]
+  }
+
+  ####################
+  ## Preprocessing ##
+  ####################
+
+  # If it's ensemble, there are multiple models, so use one for getting the categorical variables
+  if(!is.null(ensemble)){
+    ModelForNames <- model[[1]]
+  }else{
+    ModelForNames <- model
+  }
   # If there is a preprocess stage apply it, else just predict
   if(!is.null(preprocess)){
      for (i in 1:length(preprocess)){
@@ -46,21 +67,58 @@ jaqpot.predict.caret <- function(dataset, rawModel, additionalInfo){
     # Retrive the original classes of the dataset's categorical vars
     for (i in 1:dim(df)[2]){
       #Retrieve levels of factor
-      if( attr(model$terms, "dataClasses")[colnames(df)[i]] == "factor"){
+      if( attr(ModelForNames$terms, "dataClasses")[colnames(df)[i]] == "factor"){
         df[,i] <- as.factor(df[,i])
-        attributes(df[,i])$levels <- model$xlevels[colnames(df)[i]][[1]]
+        attributes(df[,i])$levels <- ModelForNames$xlevels[colnames(df)[i]][[1]]
       }
     }
-     predictions <- predict(model, df)
-
-  }else{
-     predictions <- predict(model, df)
-
   }
 
+  ################
+  ## Prediction ##
+  ################
+
+  # Retrieve ensemble column names
+  ensemble.vars <- additionalInfo$fromUser$ensemble.vars
+
+  if(!is.null(ensemble)){
+    stacking <-  matrix(rep(NA, length(model)* dim(df)[1]), ncol = length(model))
+    for (i in 1:length(model)){
+      # Select the model to use
+      UseModel <- model[[i]]
+      stacking[,i] <- predict(UseModel, df)
+    }
+    # Convert the stacked predictions matrix to dataframe and name the columns of the dataset appropriately
+    stacking <- as.data.frame(stacking)
+    colnames(stacking) <- ensemble.vars
+    # Predict using the ensemble model
+    predictions <- predict(ensemble, stacking)
+  }else{
+    predictions <- predict(model, df)
+  }
+
+
+  ######################
+  ## Detransformation ##
+  ######################
+
+  #Apply detransformation
+  ymin <- additionalInfo$fromUser$ymin
+  ymax <- additionalInfo$fromUser$ymax
+  if(length(ymin) == 1 && length(ymax) ==1){
+    for (i in 1:length(predictions)){
+      predictions[i] <- predictions[i]*(ymax-ymin)+ ymin
+    }
+  }
+
+  # Not offered to users (custom detransformation)
   if (length(extra.args)!= 0){
     predictions <- extra.args(predictions)
   }
+
+  ##################################
+  ## Name and return predictions  ##
+  ##################################
 
   for(i in 1:length(predictions)){
     prediction<- data.frame(predictions[i])
