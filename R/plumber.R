15d7c3f3c2f841dafdc4f3241830bab2c519bfd7
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-06-27 17:57:20 +0300

    fix: commit plumber R with parsers

--- R/plumber.R
+++ R/plumber.R
@@ -1,4 +1,9 @@
 library(plumber)
+
+parsers = list(json = list(simplifyVector = FALSE), rds = list())
+
 # 'plumber.R' is the location of the file shown above
 pr("predict.R") %>%
+  pr_set_parsers(parsers) %>%
   pr_run(port=8004)
+
